50423774f22f2b87373e399ff73b451c950ee356
pallavika11 <gampalapallavika@gmail.com>
2024-02-28 15:00:36 +0530

    springboot docker

---
+++ SimpleRestAPI/src/main/java/com/cisco/rest/controller/Controller.java
@@ -0,0 +1,32 @@
+package com.cisco.rest.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cisco.rest.model.Product;
+import com.cisco.rest.repo.ProductRepository;
+
+
+@RestController
+@RequestMapping("/api/product")
+public class Controller {
+	
+	@Autowired
+	ProductRepository repo;
+	@PostMapping
+	public String addNewProduct(@RequestBody Product product) {
+		repo.addProduct(product);
+		return "New product added successfully ...!!!";
+	}
+	@GetMapping
+	public List<Product> getAllProducts(){
+		return repo.getAllProducts();
+		
+	}
+}
